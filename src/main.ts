c551f2eae1d03692852921186591f606ec1adc6f
Brian Vogelgesang <KidA001@gmail.com>
2022-01-28 12:04:10 +0000

    Config changes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
 import express from 'express';
-import { rootHandler, helloHandler } from './handlers/handlers';
+import dotenv from "dotenv"
+import { rootHandler } from './handlers/handlers';
+
+if (process.env.NODE_ENV === 'development') dotenv.config({ path: './.env.local' })
 
 const app = express();
 const port = process.env.PORT || '3000';
 
 app.get('/', rootHandler);
-app.get('/hello/:name', helloHandler);
 
 app.listen(port, err => {
   if (err) return console.error(err);
-  return console.log(`Server is listening on ${port}`);
+  return console.log(`Server is running: http://localhost:${port}`);
 });
