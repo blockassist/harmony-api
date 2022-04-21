97517d3fabe68826f4e74cce643ae6734539fc62
Brian Vogelgesang <KidA001@gmail.com>
2022-04-21 08:36:05 -0700

    Temporarily remove sentry because we made them mad

--- src/lib/captureException.ts
+++ src/lib/captureException.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-console */
-import * as Sentry from '@sentry/node';
+// import * as Sentry from '@sentry/node';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function (exception: any): void {
@@ -7,8 +7,8 @@ export default function (exception: any): void {
   console.log(exception.message);
   console.log(exception);
 
-  if (process.env.ENV_NAME === 'development') return;
+  // if (process.env.ENV_NAME === 'development') return;
 
-  Sentry.init({ dsn: process.env.SENTRY_DSN });
-  Sentry.captureException(exception);
+  // Sentry.init({ dsn: process.env.SENTRY_DSN });
+  // Sentry.captureException(exception);
 }
