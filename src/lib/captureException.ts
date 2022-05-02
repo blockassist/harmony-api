3ca9288cc2606450f18765012b8d0ff2e7efd7e7
Brian Vogelgesang <KidA001@gmail.com>
2022-05-02 04:18:14 -0700

    Re-enable sentry

--- src/lib/captureException.ts
+++ src/lib/captureException.ts
@@ -1,14 +1,16 @@
 /* eslint-disable no-console */
-// import * as Sentry from '@sentry/node';
+import * as Sentry from '@sentry/node';
+
+let sentry;
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function (exception: any): void {
-  console.log('Exception Thrown:');
-  console.log(exception.message);
-  console.log(exception);
+  console.log('Exception Thrown:')
+  console.log(exception.message)
+  console.log(exception)
 
-  // if (process.env.ENV_NAME === 'development') return;
+  if (process.env.ENV_NAME !== 'production') return;
 
-  // Sentry.init({ dsn: process.env.SENTRY_DSN });
-  // Sentry.captureException(exception);
+  sentry = (sentry || Sentry.init({ dsn: process.env.SENTRY_DSN }))
+  Sentry.captureException(exception)
 }
